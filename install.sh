0d03139a45854c0fd3af2da3376a094e4ecfa567
miladprg <115424216+miladprg@users.noreply.github.com>
2024-01-20 19:40:19 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 apt install wget -y
+rm -f /usr/local/bin/ocmenu
 wget -P /usr/local/bin/ https://raw.githubusercontent.com/miladprg/ocmenu/master/ocmenu
 chmod +x /usr/local/bin/ocmenu
 ocmenu
